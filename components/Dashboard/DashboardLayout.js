f1877424339e7135f0407a2612dee2d677e96c41
vishal <vishaltyagi.dev098@gmail.com>
2022-03-17 12:44:28 +0530

    Added Dashboard

---
+++ components/Dashboard/DashboardLayout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "./Dashboard";
+
+function DashboardLayout() {
+  return (
+    <div>
+      <div className="ml-[10vw] mr-[5vw]">
+        <Dashboard />
+      </div>
+    </div>
+  );
+}
+
+export default DashboardLayout;
