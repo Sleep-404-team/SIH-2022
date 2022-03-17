54b4968430ed699f108da39a84c880b24528287c
vishal <vishaltyagi.dev098@gmail.com>
2022-03-17 13:06:22 +0530

    Added profile

--- components/Profile/ExperienceCard.js
+++ components/Profile/ExperienceCard.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import AddSection from './AddSection'
+import React from "react";
+import AddSection from "./AddSection";
 
 function ExperienceCard() {
   return (
-    <div className="text-[#1e214d]  bg-white rounded-2xl h-[75vh] px-5">
+    <div className="text-[#1e214d]  bg-white rounded-2xl h-[55vh] p-5">
       <div className="text-5xl font-bold my-4 mx-2">Experience</div>
-      <AddSection/>
+      <div className="grid grid-cols-1 gap-4">
+        <AddSection />
+        <AddSection />
+      </div>
     </div>
-  )
+  );
 }
 
-export default ExperienceCard
+export default ExperienceCard;
