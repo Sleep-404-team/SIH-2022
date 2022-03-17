7c7b7b131b8d22e7f446a5cb6051fafcf249b37e
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-03-17 11:02:50 +0530

    Internship Listing Page
    * add Footer component
    
    * add Topbar component
    
    * add InternshipListing component
    
    * add internships page
    
    * add Poppins font, change page background

---
+++ components/index.js
@@ -0,0 +1,3 @@
+export { default as Topbar } from "./Topbar";
+export { default as InternshipListings } from "./InternshipListings";
+export { default as Footer } from "./Footer";
---
+++ pages/internships/index.js
@@ -0,0 +1,23 @@
+import { Topbar, InternshipListings, Footer } from "../../components";
+
+export default function Internships({ internships }) {
+  return (
+    <>
+      <Topbar />
+      <InternshipListings internships={internships} />
+      <Footer />
+    </>
+  );
+}
+
+export const getServerSideProps = async () => {
+  const resInternships = await fetch(
+    "https://sleep404-api.vercel.app/api/v1/internship/"
+  );
+  const internships = await resInternships.json();
+  return {
+    props: {
+      internships,
+    },
+  };
+};
