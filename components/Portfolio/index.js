47f3137c8389ccb2b03bc38b3a33c69b753a976a
Mohamed Sami <srmf2025@gmail.com>
2022-03-18 15:17:16 +0530

    add Card component

--- components/Portfolio/index.js
+++ components/Portfolio/index.js
@@ -1,4 +1,5 @@
 export { default as Portfolio } from "./Portfolio";
 export { default as PortfolioHero } from "./PortfolioHero";
 export { default as ProfileCard } from "./ProfileCard";
+export { default as Card } from "./Card";
 export { default as FeaturedProjects } from "./FeaturedProjects";
